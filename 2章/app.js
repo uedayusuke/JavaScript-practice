ab35bfe256b159d512c0f5e1b11faed2640affbb
uedayusuke <uedayusuke1107@gmail.com>
2020-08-28 16:41:11 +0900

    [ADD]2章変数

--- 2章/app.js
+++ 2章/app.js
@@ -1,1 +1,20 @@
 // 2章【JavaScriptの基本文法】
+
+// 変数
+// var 変数名 = 内容;
+var hello = "Hello World!";
+alert(hello);
+
+// 整数、負数を代入
+var int1 = 1;
+var int2 = -19;
+alert(int1);
+alert(int2);
+
+// 小数点を代入
+var float1 = 3.14;
+alert(float1);
+
+// 文字列を代入
+var str1 = "本日も晴天なり";
+alert(str1);
