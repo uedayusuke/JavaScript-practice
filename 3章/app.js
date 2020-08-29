f08e8639dd36bd392a51038d523bb90f68753b4c
uedayusuke <uedayusuke1107@gmail.com>
2020-08-29 14:36:20 +0900

    [ADD]3章関数の追加

--- 3章/app.js
+++ 3章/app.js
@@ -1,1 +1,13 @@
 // 3章 【関数】
+
+// 宣言
+var alertString;
+
+// alertで呼び出し
+alert(alertString);
+
+// function 関数名(引数){処理 return}
+function addString(strA){
+	var addStr = "Hello" + strA;
+	return addStr;
+}
