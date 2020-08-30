795dbd2d7444ced2f904ee6bc8d50bf6c7a909cb
uedayusuke <uedayusuke1107@gmail.com>
2020-08-30 13:58:23 +0900

    [ADD]9章表示/非表示を追加

--- jQuery/5章/app.js
+++ jQuery/5章/app.js
@@ -1,1 +1,28 @@
 // 5章【jQueryでHTMLのCSS要素を操作しよう】
+// box1を青色長方形に変更
+$(function(){
+	$(".box1").css({
+		"background-color": "#0000FF",
+		"height": "100px"
+	});
+});
+
+// box2を上から下へスライド
+$(function(){
+	$(".box2").slideDown();
+});
+
+// box3を下から上のスライド
+$(function(){
+	$(".box3").slideUp();
+});
+
+// box4を非表示から表示
+$(function(){
+	$(".box4").show();
+});
+
+// box5を表示から非表示
+$(function(){
+	$(".box5").hide("slow");
+});
