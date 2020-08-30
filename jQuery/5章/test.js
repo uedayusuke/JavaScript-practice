0c6f9f1c3d3d41f46462dd580ab8a293403576bf
uedayusuke <uedayusuke1107@gmail.com>
2020-08-30 14:17:54 +0900

    [FINISH]9章確認問題

--- jQuery/5章/test.js
+++ jQuery/5章/test.js
@@ -1,1 +1,25 @@
 // 5章確認問題
+
+// boxを青色長方形に変更
+$(function(){
+	$(".box").css({
+		"background-color": "blue",
+		"width": "200px"
+	});
+});
+
+// boxを下から上にスライド
+$(function(){
+	$(".box").slideUp();
+});
+
+
+// boxを1秒かけて再表示
+$(function(){
+	$(".box").show(1000);
+});
+
+// boxをf1.5秒けてadeout
+$(function(){
+	$(".box").fadeOut(1500);
+});
