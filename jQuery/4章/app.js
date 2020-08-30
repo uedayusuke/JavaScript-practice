cd107ab35c33f28fb3ece2867cdb0d4115dfa821
uedayusuke <uedayusuke1107@gmail.com>
2020-08-30 12:45:25 +0900

    [ADD]jQueryを記述

--- jQuery/4章/app.js
+++ jQuery/4章/app.js
@@ -1,4 +1,25 @@
 // 4章【jQueryを使ってみよう】
+/*
+$(document).ready(function(){
+	jQueryプログラムの内容
+})
+↓省略形
+$(function(){
+	jQueryプログラムの内容
+})
+*/
+/*
+プログラムの書き方
+$(セレクタ).メソッド.(パラメーター);
+セレクタは対象箇所
+メソッドは何を行うのか
+パラメーターは内容
+→"どこに何をするか"を記述
+*/
 $(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+	$('body').html('<h1>Hello jQuery!!</h1>');
 });
+
+$(function(){
+	$("body").html("<h2>省略形</h2>");
+})
