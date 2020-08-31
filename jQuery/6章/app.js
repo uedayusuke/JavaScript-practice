b58dda12e14559432c7a91c1613baae6bba9cfad
uedayusuke <uedayusuke1107@gmail.com>
2020-08-31 19:43:56 +0900

    [ADD]6章イベントの追加

--- jQuery/6章/app.js
+++ jQuery/6章/app.js
@@ -1,1 +1,20 @@
 // 6章【イベントを検出しよう】
+
+// mouseover
+// マウスが乗ると青色に変更
+/*
+$("セレクタ").イベント(function(){
+	イベント発生時の処理
+});
+*/
+$(function(){
+	$(".box1").mouseover(function(){
+		$(".box1").css({"background-color": "#0000FF"});
+	});
+	// mouseout
+	// マウスが離れたら赤に戻る
+	$(".box1").mouseout(function(){
+		$(".box1").css({"background-color": "#FF0000"});
+	});
+});
+
