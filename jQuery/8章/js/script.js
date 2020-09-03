efaa8c4450f6f1b255fd4d3039bcb010a235c953
uedayusuke <uedayusuke1107@gmail.com>
2020-09-03 21:10:49 +0900

    [ADD]8章パラメーターを設定

--- jQuery/8章/js/script.js
+++ jQuery/8章/js/script.js
@@ -1,1 +1,14 @@
 // 8章【jQueryのプラグインを導入しよう】
+
+$(".theTarget").skippr({
+	transition : "fade",
+	speed : 1000,
+	easing : "easeOutQuart",
+	navType : "block",
+	childrenElementType : "div",
+	arrows : false,
+	autoPlay : true,
+	autoPlayDuration : 2000,
+	keyboardOnAlways : true,
+	hidePrevious : false
+});
