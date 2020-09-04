0bed300382e265942f4575ac6d1eadacc7960976
uedayusuke <uedayusuke1107@gmail.com>
2020-09-04 15:49:54 +0900

    [ADD]アニメーションの追加

--- jQuery/9章/js/script.js
+++ jQuery/9章/js/script.js
@@ -1,1 +1,12 @@
 // 9章【トップに戻るを作ってみよう】
+
+$(function(){
+	// "calss"ではなく"id"のため$("#id名 要素名")で指定
+	$("#back a").on("click", function(event){
+		// #backの<a>タグがクリックされた時の処理
+		$("body, html").animate({
+			scrollTop:0
+		}, 800);
+		event.preventDefault();
+	});
+});
