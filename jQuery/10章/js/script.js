6cffe7e67a7cf2f35c395bad029471a12cb551d1
uedayusuke <uedayusuke1107@gmail.com>
2020-09-08 19:11:07 +0900

    [ADD]10章クリックイベントを追加

--- jQuery/10章/js/script.js
+++ jQuery/10章/js/script.js
@@ -1,7 +1,11 @@
 // 10章【ハンバーガーメニューを作ってみよう】
 $(function(){
 	$(".menu-trigger").on("click", function(event){
+		// menu-triggerをクリックした時のアクション
+		// thisは要素（menu-trigger）を指す
+		// toggleClassでクリックされた時"active"が追加され、もう一度クリックすると削除される
 		$(this).toggleClass("active");
+		// fadeToggleで要素をフェードアウトさせる
 		$("#sp-menu").fadeToggle();
 		event.preventDefault();
 	});
