4a3f213f99cf4642d16eafedb11a59ed5f15a85c
uedayusuke <uedayusuke1107@gmail.com>
2020-09-08 19:01:23 +0900

    [ADD]10章クリックイベントを追加

--- jQuery/10章/js/script.js
+++ jQuery/10章/js/script.js
@@ -1,1 +1,8 @@
 // 10章【ハンバーガーメニューを作ってみよう】
+$(function(){
+	$(".menu-trigger").on("click", function(event){
+		$(this).toggleClass("active");
+		$("#sp-menu").fadeToggle();
+		event.preventDefault();
+	});
+});
